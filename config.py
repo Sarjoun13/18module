d1da97ae6e37b732cc4dc0c0190b16027f434e58
Sarjoun13 <sarjoun13@mail.ru>
2022-07-29 22:08:22 +0300

    new files for practice

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '5489686262:AAGzo4PtiPDAqCgBZx50cdKz0L7F-YyYHKk'
+
+keys = {
+    'биткоин' : 'BTC',
+    'эфириум' : 'ETH',
+    'доллар' : 'USD',
+    'евро' : 'EUR',
+    'рубль' : 'RUB'
+}
+
